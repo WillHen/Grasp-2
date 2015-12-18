af40c4b53829828e32453042d753d35e7d6c5458
William Henshaw <whenshaw87@googlemail.com>
2015-12-17 20:10:36 -0800

    Added database stuff

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,14 @@
 //sets up database connection
 var pg = require('knex')({
   client: 'pg',
-  connection: process.env.DATABASE_URL || 'postgres://localhost:5432/graspdb',
+  // connection: process.env.DATABASE_URL || 'postgres://localhost:5432/graspdb',
+  connection: {
+    host     : process.env.DATABASE_URL || 'localhost',
+    user     : process.env.DATABASE_USER || '',
+    password : process.env.DATABASE_PASSWORD || '',
+    database : 'graspdb',
+    port: 5432
+  },
   searchPath: 'knex,public'
 });
 
